a25a6506e100a5430b9878ebbada5a9011df21bb
Mateus Santis <mateussantiss@gmail.com>
2019-09-13 14:06:12 -0300

    exercicio1matelucy

---
+++ Aula1/Exercicios/Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numero;
+
+            Console.Write("Digite um numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            int triplo = numero * 3;
+
+            Console.WriteLine();
+
+            Console.WriteLine(triplo);
+
+        }
+    }
+}
