ec64c5ee12d64c90fe04cac1ee8d7c898a7ad240
Mateus Santis <mateussantiss@gmail.com>
2019-09-16 11:23:38 -0300

    Att if else

---
+++ Aula1/Exercicios/Ex8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1, valor2, valor3, media;
+
+            Console.Write("Escreva o numero: ");
+            valor1 = double.Parse(Console.ReadLine());
+            Console.Write("Escreva o numero: ");
+            valor2 = double.Parse(Console.ReadLine());
+            Console.Write("Escreva o numero: ");
+            valor3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            media = (valor1 + valor2 + valor3) / 3;
+            Console.WriteLine("Sua media final foi de: " +media);
+
+            if(media >= 6) {
+                Console.WriteLine("Passou de ano!");
+            }
+            else {
+                Console.WriteLine("Reprovado!");
+            }
+        }
+    }
+}
