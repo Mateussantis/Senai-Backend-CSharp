ebe627143204d8cac0b799ded818e39ca22ba54f
Mateus Santis <mateussantiss@gmail.com>
2019-09-24 11:57:03 -0300

    Funções

---
+++ Aula4/Ex8/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = new String [5];
+            String[] origem = new String [5];
+            String[] destino = new String [5];
+            DateTime[] data_ida = new DateTime [5];
+            DateTime[] data_volta = new DateTime [5];
+            string sair = "";
+
+            while(sair != "0") {    
+                
+            Console.WriteLine("\nMenu\n");
+            Console.WriteLine("1 - Cadastrar voo!");
+            Console.WriteLine("2 - Listar Infos!");
+            Console.WriteLine("0 - Sair!\n");
+
+            Console.Write("\nDigite uma opção: ");
+            sair = Console.ReadLine();
+
+            switch (sair) {
+
+                case "1" :
+                    for (int i = 0; i <= 4; i++) {
+                        
+                        Console.WriteLine("");
+
+                        Console.Write($"Digite o {i+1} nome: ");
+                        nome[i] = Console.ReadLine();
+
+                        Console.Write($"Digite o {i+1} origem: ");
+                        origem[i] = Console.ReadLine();
+
+                        Console.Write($"Digite a {i+1} destino: ");
+                        destino[i] = Console.ReadLine();
+
+                        Console.Write($"Digite o {i+1} data: ");
+                        data_ida[i] =   DateTime.Parse(Console.ReadLine());
+
+                        Console.Write($"Digite o {i+1} data: ");
+                        data_volta[i] = DateTime.Parse(Console.ReadLine());
+                }                                        
+                break;
+
+                case "2" :
+                    for (int i = 0; i <= 4; i++) {
+                        if (nome[i] != "" && origem[i] != "" && destino[i] != "" && data_ida[i] != null && data_volta[i] != null) {
+                        Console.WriteLine($"O passageiro {i+1}");
+                        Console.WriteLine("Passageiro: " +nome[i]);
+                        Console.WriteLine("A origem: " +origem[i]);
+                        Console.WriteLine("O destino é: " +destino[i]);
+                        Console.WriteLine("A data de ida é: " +data_ida[i]);
+                        Console.WriteLine("A data de volta é:" +data_volta[i]);
+                        Console.WriteLine("");
+                        }
+                        else {
+                            Console.WriteLine($"Erro, no usuario {i+1}\n");
+                        }
+                        
+                }  
+                break;
+
+                default:
+                    Console.WriteLine("Voce acabou de sair do programa");
+                break;
+            }
+        }
+            
+        }
+    }
+}
---
+++ Aula6/Exemplo1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] valores = new double[3];
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= 2; i++) {
+                Console.Write($"Digite o {i+1} numero: ");
+                valores[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nA media desses valores é: " +calculamedia(valores)); 
+            Console.WriteLine("A media desses valores é: " +somavalores(valores)); 
+            Console.WriteLine();
+        }
+        
+        /// <summary>
+        /// Função que escreve bom dia para o usuario;
+        /// </summary>
+        /// <returns>Retorna, Olá, bom dia;</returns>
+        static string Bomdia() {
+            return "Olá. Bom Dia"; 
+        }
+
+        /// <summary>
+        /// Função que retorna uma saudação de acordo com o horario
+        /// </summary>
+        /// <param name="saudação">De acordo com o horario</param>
+        /// <returns></returns>
+        static string Bomdia(string saudação) {
+           int hora = DateTime.Now.Hour;
+
+           if (hora <= 12 && hora >= 6) {
+               saudação = "Bom Dia";
+           }
+           else if (hora > 12 && hora <= 18) {
+               saudação = "Boa Tarde";
+           }
+           else {
+               saudação = "Boa noite";
+           }
+            return saudação;
+        }
+
+        /// <summary>
+        /// Retorna um numero
+        /// </summary>
+        /// <param name="numero">Inteiro qualquer</param>
+        /// <returns>Numero passado como paramentro</returns>
+        static int Bomdia(int numero) {
+            return numero;
+        }
+
+        /// <summary>
+        /// Fução que imprimi a data e a hora;
+        /// </summary>
+        static void ImprimiDataHora() {
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.Hour);
+        }
+
+
+        /// <summary>
+        /// Função que calcula a media de um vetor
+        /// </summary>
+        /// <param name="valores">Vetor com valores</param>
+        /// <returns>Media dos valores</returns>
+        static double calculamedia(double[] valores) {
+            double resultado = 0;
+
+            for (int i = 0; i < valores.Length; i++) {
+                resultado += valores[i];
+            }
+
+            resultado = resultado / valores.Length;
+
+            return resultado;
+        }
+
+        /// <summary>
+        /// Soma os valores desse vetor
+        /// </summary>
+        /// <param name="valores">Vetor</param>
+        /// <returns>Soma dos valores</returns>
+        static double somavalores(double[] valores) {
+            double resultado = 0;
+
+            for (int i = 0; i < valores.Length; i++) {
+                resultado += valores[i];
+            }
+            return resultado;
+        }
+    }
+}
